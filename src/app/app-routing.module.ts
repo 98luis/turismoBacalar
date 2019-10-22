27f1e1bfff8a8cf5eac92006f3aca21641fa3ba1
Luis <2016011363@upb.edu.mx>
2019-10-22 12:19:45 -0500

    1
    una app de turismo

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  { path: 'informacion', loadChildren: './informacion/informacion.module#InformacionPageModule' },
+  { path: 'turisticos', loadChildren: './turisticos/turisticos.module#TuristicosPageModule' }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
